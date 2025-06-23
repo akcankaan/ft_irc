3c0d5ba7f6245c317694ead86f97a98283791996
Ibrahim Ecer <iecer@k2m17s07.42kocaeli.com.tr>
2025-06-23 14:56:59 +0300

    23062025

--- command/commands/nick.cpp
+++ command/commands/nick.cpp
@@ -4,9 +4,11 @@
 #include <sstream>
 #include <sys/socket.h>
 
+
 void nick(Client *client, std::istringstream &iss)
 {
     std::string nickname;
+    static std::string oldNick;
     iss >> nickname;
 
     if (client->shouldDisconnect())
@@ -18,12 +20,13 @@ void nick(Client *client, std::istringstream &iss)
     Server* server = client->getServer();
 
     if (server->isNicknameInUse(nickname)) {
+        oldNick = nickname;
         std::string msg = ":server 433 * " + nickname + " :Nickname is already in use\r\n";
         send(client->getFd(), msg.c_str(), msg.length(), 0);
         return;
     }
-
-    std::string oldNick = client->getNickname();
+    if (client->getNickname().length() != 0)
+        oldNick = client->getNickname();
     client->setNickname(nickname);
 
     std::string msg = ":" + oldNick + "!" + client->getUsername() + "@localhost NICK :" + nickname + "\r\n";
