7d766b89e44475af559146dfad61a9d598757048
Ibrahim Ecer <iecer@k2m17s07.42kocaeli.com.tr>
2025-06-25 18:11:27 +0300

    25062025

--- command/commands/nick.cpp
+++ command/commands/nick.cpp
@@ -2,15 +2,16 @@
 #include "../../server/Server.hpp"
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 #include <sys/socket.h>
 
 
 void nick(Client *client, std::istringstream &iss)
 {
     std::string nickname;
+    static int i = 0;
     static std::string oldNick;
     iss >> nickname;
-
     if (client->shouldDisconnect())
         return;
 
@@ -20,7 +21,9 @@ void nick(Client *client, std::istringstream &iss)
     Server* server = client->getServer();
 
     if (server->isNicknameInUse(nickname)) {
-        oldNick = nickname;
+        if (i == 0)
+            oldNick = nickname;
+        i = 1;
         std::string msg = ":server 433 * " + nickname + " :Nickname is already in use\r\n";
         send(client->getFd(), msg.c_str(), msg.length(), 0);
         return;
@@ -31,7 +34,28 @@ void nick(Client *client, std::istringstream &iss)
 
     std::string msg = ":" + oldNick + "!" + client->getUsername() + "@localhost NICK :" + nickname + "\r\n";
 
+    std::map<std::string, Channel *> &channels = server->getChannelMap();
+    std::vector<Channel*> userChannels;
+    for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); ++it)
+    {
+        Channel* channel = it->second;
+        const std::vector<Client*>& clist = channel->getClients();
+        if (std::find(clist.begin(), clist.end(), client) != clist.end())
+            userChannels.push_back(channel);
+    }
+    for (size_t i = 0; i < userChannels.size(); ++i)
+    {
+        Channel* channel = userChannels[i];
+        std::string chanName = channel->getName();
+        bool wasOperator = channel->isOperator(oldNick);
+        std::cout << wasOperator << std::endl;
+        if (wasOperator)
+        {
+            channel->removeOperator(oldNick);
+            channel->addOperator(nickname);
+        }
+        channel->broadcast(msg, NULL);
+    }
     send(client->getFd(), msg.c_str(), msg.length(), 0);
-
-    std::cout << "[DEBUG] Nick change: " << oldNick << " -> " << nickname << std::endl;
+    i = 0;
 }
--- command/commands/quit.cpp
+++ command/commands/quit.cpp
@@ -4,45 +4,73 @@
 #include <iostream>
 #include <sstream>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <algorithm>
 
 
 void quit(Client *client, std::istringstream &iss)
 {
-	std::string chanName;
-	iss >> chanName;
+    std::string dummy; // Artık tek bir kanal adı değil, quit mesajı parse edilecek
+    iss >> dummy; // Çoğu zaman boş gelir
 
-	std::string msg;
-	std::getline(iss, msg);
-	if (client->shouldDisconnect())
-		return;
-	if (!msg.empty() && msg[0] == ' ')
-		msg.erase(0, 1);
+    std::string msg;
+    std::getline(iss, msg);
 
-	if (!msg.empty() && msg[0] == ':')
-		msg.erase(0, 1);
+    if (client->shouldDisconnect())
+        return;
 
-	if (msg.empty())
-		msg = "Client exited";
+    if (!msg.empty() && msg[0] == ' ')
+        msg.erase(0, 1);
+    if (!msg.empty() && msg[0] == ':')
+        msg.erase(0, 1);
+    if (msg.empty())
+        msg = "Client exited";
 
-	Server *server = client->getServer();
-	std::map<std::string, Channel *> &channels = server->getChannelMap();
+    Server *server = client->getServer();
+    std::map<std::string, Channel *> &channels = server->getChannelMap();
 
-	if (channels.find(chanName) == channels.end())
-	{
-		send(client->getFd(), "No such channel\r\n", 18, 0);
-	}
+    std::string quitMsg = ":" + client->getNickname() + "!" + client->getUsername() +
+                          "@localhost QUIT :" + msg + "\r\n";
 
-for (std::map<std::string, Channel*>::iterator it = channels.begin(); it != channels.end(); ++it) {
-        std::string quitMsg = ":" + client->getNickname() + "!" + client->getUsername() +
-		"@localhost QUIT :" + msg + "\r\n"; // IRC QUIT protokolüne daha uygun
+    // Kullanıcının içinde bulunduğu tüm kanalları bul
+    std::vector<Channel*> userChannels;
+    for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); ++it)
+    {
+        Channel* channel = it->second;
+        const std::vector<Client*>& clist = channel->getClients();
+        if (std::find(clist.begin(), clist.end(), client) != clist.end())
+            userChannels.push_back(channel);
+    }
 
+    for (size_t i = 0; i < userChannels.size(); ++i)
+    {
+        Channel* channel = userChannels[i];
+        std::string chanName = channel->getName();
 
-        Channel* channel = it->second; // Tek değişken ile çalış
+        bool wasOperator = channel->isOperator(client->getNickname());
+
+        // Client'ı kanaldan sil
         channel->removeClient(client);
+
+        // QUIT mesajı
         channel->broadcast(quitMsg, client);
-        break; // Bulduktan sonra çık
-}
 
+        // Eğer çıkan kullanıcı operator ise ve kanalda başka kullanıcı varsa
+        if (wasOperator && !channel->getClients().empty())
+        {
+            // Son giren kullanıcıyı bul (en sondaki)
+            Client* newOp = channel->getClients().back();
+            channel->addOperator(newOp->getNickname());
 
-	std::cout << client->getNickname() << " exited from " << chanName << " channel" << std::endl;
+            std::string modeMsg = ":@localhost MODE " + chanName +
+                " +o " + newOp->getNickname() + "\r\n";
+            channel->broadcast(modeMsg, NULL);
+        }
+
+        std::cout << client->getNickname() << " exited from " << chanName << " channel" << std::endl;
+    }
+
+    // Server'dan çıkarma işlemleri (isteğe bağlı, aşağıdaki satırı kendi sistemine göre düzenle)
+    server->removeClient(client->getFd()); // Böyle bir fonksiyonun varsa kullanırsın.
+
 }
